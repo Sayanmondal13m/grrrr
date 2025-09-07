5a74cbc7b915309472e320effdca29a0cfca49a7
Sayan Mondal <sm187966@gmail.com>
2025-09-07 08:24:49 +0000

    ok now evrything is works good but for desktop evrything is but in phone

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,78 +1,58 @@
-// DO NOT EDIT
-// This file is a copy of a file that is downloaded from the web.
-// The original can be found at https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-sw.js
 
-importScripts(
-  'https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js'
-);
-importScripts(
-  'https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js'
-);
+// Give the service worker access to Firebase Messaging.
+// Note that you can only use Firebase Messaging here, other Firebase libraries
+// are not available in the service worker.
+import { initializeApp } from 'firebase/app';
+import { getMessaging } from 'firebase/messaging/sw';
+import { onBackgroundMessage } from 'firebase/messaging/sw';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyAowX6z6IDuosoxlfclYkgof5HXC27UEmA',
-  authDomain: 'garena-gears.firebaseapp.com',
-  projectId: 'garena-gears',
-  storageBucket: 'garena-gears.firebasestorage.app',
-  messagingSenderId: '93335858315',
-  appId: '1:93335858315:web:9ef6be42c3b81a236ab88e',
-  measurementId: 'G-MEASUREMENT_ID',
+  projectId: "garena-gears",
+  appId: "1:93335858315:web:9ef6be42c3b81a236ab88e",
+  storageBucket: "garena-gears.firebasestorage.app",
+  apiKey: "AIzaSyAowX6z6IDuosoxlfclYkgof5HXC27UEmA",
+  authDomain: "garena-gears.firebaseapp.com",
+  measurementId: "",
+  messagingSenderId: "93335858315"
 };
 
-const app = firebase.initializeApp(firebaseConfig);
-const messaging = firebase.messaging();
+const app = initializeApp(firebaseConfig);
+const messaging = getMessaging(app);
 
-// This is the listener for background push notifications.
-messaging.onBackgroundMessage(function (payload) {
-  console.log(
-    '[firebase-messaging-sw.js] Received background message ',
-    payload
-  );
+onBackgroundMessage(messaging, (payload) => {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
 
-  // Instead of a 'notification' payload, we now expect a 'data' payload.
-  // This gives us full control over displaying the notification.
-  const notificationData = payload.data;
-  if (!notificationData) {
-      console.error("No data payload received in background message.");
-      return;
-  }
-
-  const notificationTitle = notificationData.title;
+  const notificationTitle = payload.data.title;
   const notificationOptions = {
-    body: notificationData.body,
-    icon: '/img/garena.png', // Default icon
-    image: notificationData.image, // Optional image
-    data: { // Add data to be retrieved when notification is clicked
-        url: notificationData.link || '/'
-    }
+    body: payload.data.body,
+    icon: '/img/garena.png',
+    image: payload.data.image,
+    badge: '/img/garena-badge.png',
+    data: {
+        url: payload.data.link || '/',
+    },
   };
 
   self.registration.showNotification(notificationTitle, notificationOptions);
 });
 
+self.addEventListener('notificationclick', (event) => {
+  event.notification.close();
 
-// Handle notification click event
-self.addEventListener('notificationclick', function(event) {
-  event.notification.close(); // Close the notification
-
-  const urlToOpen = event.notification.data.url;
+  const urlToOpen = event.notification.data.url || '/';
 
   event.waitUntil(
-    clients.matchAll({
-      type: 'window',
-      includeUncontrolled: true
-    }).then(function(windowClients) {
-      // Check if there is already a window/tab open with the target URL
-      for (var i = 0; i < windowClients.length; i++) {
-        var client = windowClients[i];
-        if (client.url === urlToOpen && 'focus' in client) {
-          return client.focus();
+    clients.matchAll({ type: 'window', includeUncontrolled: true }).then((clientList) => {
+      if (clientList.length > 0) {
+        let client = clientList[0];
+        for (let i = 0; i < clientList.length; i++) {
+          if (clientList[i].focused) {
+            client = clientList[i];
+          }
         }
+        return client.focus().then(c => c.navigate(urlToOpen));
       }
-      // If not, open a new window/tab
-      if (clients.openWindow) {
-        return clients.openWindow(urlToOpen);
-      }
+      return clients.openWindow(urlToOpen);
     })
   );
 });
