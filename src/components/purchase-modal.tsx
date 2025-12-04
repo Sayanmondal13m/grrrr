d5177e47d8bf56919fb1c49c9399b9bf99ae5832
Sayan Mondal <sm187966@gmail.com>
2025-12-04 14:46:01 +0000

    look the current code is fine now teach the ai that user can wait if the

--- src/components/purchase-modal.tsx
+++ src/components/purchase-modal.tsx
@@ -420,7 +420,7 @@ export default function PurchaseModal({ product, user: initialUser, onClose }: P
                                         viewBox={`0 0 176 176`}
                                     />
                                     <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-white p-1 rounded-md">
-                                        <Image src="/img/upi.png" alt="UPI Logo" width={32} height={32} />
+                                        <Image src="/img/upilogo.png" alt="UPI Logo" width={32} height={32} />
                                     </div>
                                 </div>
                             )}
@@ -470,8 +470,8 @@ export default function PurchaseModal({ product, user: initialUser, onClose }: P
                                 <Image src="/img/mobikwik.png" alt="MobiKwik" width={28} height={28} />
                                 MobiKwik
                             </div>
-                            <div className="flex flex-col items-center justify-start gap-3 text-[10px] text-muted-foreground">
-                                <Image src="/img/upi.png" alt="Other UPI Apps" width={58} height={58} />
+                            <div className="flex flex-col items-center justify-start gap-1 text-[10px] text-muted-foreground">
+                                <Image src="/img/upilogo.png" alt="Other UPI Apps" width={28} height={28} />
                                 Other Apps
                             </div>
                         </div>
